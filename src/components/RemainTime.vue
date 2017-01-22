50c069bf82ab089065d65a9a641fb1ef45b0d438
moka-a <aud1220a@gmail.com>
2017-01-22 17:07:30 +0900

    addjust wallpager star paticle

---
+++ src/components/RemainTime.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ dueDate }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      dueDate: {
+        type: String,
+        required: true,
+        default: 20170301
+      },
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,58 @@
 <template>
-  <main-layout>
-    <p>Welcome home</p>
-  </main-layout>
+  <header id="top" class="header">
+    <div class="title">
+      <h2>DroidKnights 2017 in Seoul</h2>
+    </div>
+
+    <div class="text-vertical-center">
+      <h1>Comming Soon ..</h1>
+      <br>
+      <remain-time dueDate="bbbbbbbb"></remain-time>
+    </div>
+  </header>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
+  import RemainTime from '../components/RemainTime.vue'
 
   export default {
+    created() {
+    },
     components: {
-      MainLayout
+      RemainTime
     }
   }
 </script>
+
+<style scoped>
+  .header {
+    display: table;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    background-size: cover;
+    -o-background-size: cover;
+  }
+
+  .title {
+    display: table-header-group;
+    padding-left: 100px;
+    color: white;
+  }
+
+  .text-vertical-center {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+    color: white;
+  }
+
+  .text-vertical-center h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 4.5em;
+    font-weight: 700;
+  }
+</style>
