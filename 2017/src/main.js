2c8dc63ff068ccc986706599ae165074c1c216d9
moka-a <aud1220a@gmail.com>
2017-02-28 11:02:49 +0900

    불필요한것 삭제

--- 2017/src/main.js
+++ 2017/src/main.js
@@ -2,15 +2,8 @@ import Vue from 'vue'
 import Rx from 'rxjs/Rx'
 import VueRx from 'vue-rx'
 import router from './router'
-import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueRx, Rx)
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyDvFB_V9GZguW_JIrUK55_3KWoTYThgy1E',
-    v: '3.26'
-  }
-})
 
 new Vue({
   router
