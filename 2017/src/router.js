cb30ed26a85828ca502c739aad49a53c246fa8b1
moka-a <aud1220a@gmail.com>
2017-03-03 15:03:53 +0900

    rollback

--- 2017/src/router.js
+++ 2017/src/router.js
@@ -1,28 +1,28 @@
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-//
-// Vue.use(VueRouter)
-//
-// /* Layouts */
-// import Home from './pages/Home.vue'
-// import Schedule from './pages/Schedule.vue'
-//
-// const router = new VueRouter({
-//   routes: [
-//     {
-//       path: '/',
-//       component: Schedule
-//     },
-//     {
-//       path: '/schedule',
-//       component: Schedule
-//     }
-//   ]
-// })
-//
-// export default router
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-export default {
-  '/': 'Schedule',
-  '/2017': 'Schedule'
-}
+Vue.use(VueRouter)
+
+/* Layouts */
+import Home from './pages/Home.vue'
+import Schedule from './pages/Schedule.vue'
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Schedule
+    },
+    {
+      path: '/schedule',
+      component: Schedule
+    }
+  ]
+})
+
+export default router
+
+// export default {
+//   '/': 'Schedule',
+//   '/2017': 'Schedule'
+// }
