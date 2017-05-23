1d37a74bc5ddf805f3146dadd768d600f9761f0a
moka-a <aud1220a@gmail.com>
2017-05-23 14:31:08 +0900

    fix build fail

--- _site/2017/node_modules/vue2-google-maps/dist/components/autocomplete.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <input type="text" :placeholder="placeholder" ref="input"
-    :value="value"/>
-</template>
-
-<script src="./autocompleteImpl.js">
-</script>
--- _site/2017/node_modules/vue2-google-maps/src/utils/getPropsValuesMixin.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash'
-
-export default {
-  methods: {
-    getPropsValues () {
-      return _.mapValues(this.$options.props, (v, k) => this[k]);
-    }
-  }
-}
