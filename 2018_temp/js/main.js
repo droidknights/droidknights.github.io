254737aabca3e2431d64667c025919aab29d6794
이영찬 <leeyc09@gmail.com>
2018-03-09 15:02:28 +0900

    temp 2018 page

---
+++ 2018_temp/js/main.js
@@ -0,0 +1,162 @@
+$(window).on("load", function() {
+    $("#preloader").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+
+     new WOW().init();
+
+
+     $("#top-nav").onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
+
+     
+    //animated header class
+    $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 200) {
+        //console.log('a');
+        $(".navigation").addClass("animated");
+    } else {
+        //console.log('a');
+        $(".navigation").removeClass("animated");
+    }});
+
+    $("#countdown_dashboard").countDown({
+        targetDate: {
+            'day':      22,
+            'month':    4,
+            'year':     2018,
+            'hour':     09,
+            'min':      00,
+            'sec':      00,
+        },
+        omitWeeks: true
+    });
+
+    $(".about-slider").owlCarousel(
+        {
+        singleItem: true,
+        pagination : true,
+        autoPlay : 5000,
+        }
+    );
+
+    /*$('input, textarea').data('holder', $('input, textarea').attr('placeholder'));
+
+    $('input, textarea').focusin(function () {
+        $(this).attr('placeholder', '');
+    });
+    $('input, textarea').focusout(function () {
+        $(this).attr('placeholder', $(this).data('holder'));
+    });*/
+
+
+    //contact form validation
+    $("#contact-form").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            message: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter Your Name",
+                minlength: "Your name must consist of at least 2 characters"
+            },
+            message: {
+                required: "Please Write Something",
+                minlength: "Your message must consist of at least 2 characters"
+            },
+            email: "Please enter a valid email address"
+        },
+        submitHandler: function(form) {
+            $(form).ajaxSubmit({
+                type:"POST",
+                data: $(form).serialize(),
+                url:"mail.php",
+                success: function() {
+                    $("#contact-form :input").attr('disabled', 'disabled');
+                    $("#contact-form").fadeTo( "slow", 0.15, function() {
+                        $(this).find(':input').attr('disabled', 'disabled');
+                        $(this).find('label').css('cursor','default');
+                        $("#success").fadeIn();
+                    });
+                },
+                error: function() {
+                    $("#contact-form").fadeTo( "slow", 0.15, function() {
+                        $("#error").fadeIn();
+                    });
+                }
+            });
+        }
+    });
+
+});
+
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(37.512081, 127.059800);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        // var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+        // map.set('styles', styles);
+
+        var markerImage = 'images/marker.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>Info Window Content</h3>' +
+                '<div class="info-content">' +
+                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
+
+
+
+
+
+
