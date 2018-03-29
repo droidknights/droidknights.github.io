e3fe735054a0019d4286f521e45f9da2b7afea3b
강경완 <kw.kang@woowahan.com>
2018-03-29 16:52:46 +0900

    OnePage Nav exteranl link 예외 처리

--- 2018/js/jquery.nav.js
+++ 2018/js/jquery.nav.js
@@ -39,7 +39,7 @@
 			currentClass: 'current',
 			changeHash: false,
 			easing: 'swing',
-			filter: '',
+			filter: ':not(.external)',
 			scrollSpeed: 750,
 			scrollThreshold: 0.5,
 			begin: false,
