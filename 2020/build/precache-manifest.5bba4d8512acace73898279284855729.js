54f5ee69c659b98a515892380edae5c5ac23cc4a
dudco <dudco1129@naver.com>
2020-01-21 11:40:35 +0900

    배포

---
+++ 2020/build/precache-manifest.5bba4d8512acace73898279284855729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b46c179d9c53560ae90a89caa2afede",
+    "url": "/2020/build/index.html"
+  },
+  {
+    "revision": "c721f51016162b0fd9b4",
+    "url": "/2020/build/static/css/main.6a3ea716.chunk.css"
+  },
+  {
+    "revision": "b158948954830b58cb18",
+    "url": "/2020/build/static/js/2.cac97926.chunk.js"
+  },
+  {
+    "revision": "c721f51016162b0fd9b4",
+    "url": "/2020/build/static/js/main.784f422b.chunk.js"
+  },
+  {
+    "revision": "71f3d817c9e6ec44bdb1",
+    "url": "/2020/build/static/js/runtime-main.ca0e5ff3.js"
+  },
+  {
+    "revision": "aa3a0087b77a9d4bd2520c86f47eb639",
+    "url": "/2020/build/static/media/BIG_JOHN.aa3a0087.otf"
+  }
+]);
