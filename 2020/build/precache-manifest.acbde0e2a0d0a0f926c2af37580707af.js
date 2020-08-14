0b485d8d4d6f7c0572dbe900490d05b295708e0a
dudco <dudco1129@naver.com>
2020-08-14 11:18:02 +0900

    build

---
+++ 2020/build/precache-manifest.acbde0e2a0d0a0f926c2af37580707af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de48ee007df2dffb42e68535d8fad693",
+    "url": "/2020/build/index.html"
+  },
+  {
+    "revision": "93b6d24821d53fdb344c",
+    "url": "/2020/build/static/css/main.6c66fe89.chunk.css"
+  },
+  {
+    "revision": "a1e46d69d58a14747a00",
+    "url": "/2020/build/static/js/2.b25467e2.chunk.js"
+  },
+  {
+    "revision": "93b6d24821d53fdb344c",
+    "url": "/2020/build/static/js/main.711e1776.chunk.js"
+  },
+  {
+    "revision": "71f3d817c9e6ec44bdb1",
+    "url": "/2020/build/static/js/runtime-main.ca0e5ff3.js"
+  },
+  {
+    "revision": "aa3a0087b77a9d4bd2520c86f47eb639",
+    "url": "/2020/build/static/media/BIG_JOHN.aa3a0087.otf"
+  },
+  {
+    "revision": "c4723de3da750cff5d95b7c9b398d983",
+    "url": "/2020/build/static/media/NotoSansKR-Bold.c4723de3.otf"
+  },
+  {
+    "revision": "839a2f6c607e26dee10678a077729ba5",
+    "url": "/2020/build/static/media/NotoSansKR-Regular.839a2f6c.otf"
+  }
+]);
--- 2020/build/service-worker.js
+++ 2020/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2020/build/precache-manifest.03da84947fbf939111815df4ca3de29e.js"
+  "/2020/build/precache-manifest.acbde0e2a0d0a0f926c2af37580707af.js"
 );
 
 self.addEventListener('message', (event) => {
